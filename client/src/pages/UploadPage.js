606604cfa50752781d3c4a93cea415b29569212e
Gustavo Felix <gfnascimento@inf.ufsm.br>
2023-09-08 20:33:04 -0300

    Integrando file picker

--- client/src/pages/UploadPage.js
+++ client/src/pages/UploadPage.js
@@ -1,6 +1,7 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import PageWrapper from '../components/PageWrapper';
 import { useDropzone } from 'react-dropzone';
+import { useFilePicker } from 'use-file-picker';
 import styled from 'styled-components';
 import Papa from 'papaparse'
 
@@ -21,18 +22,15 @@ const DropInput = styled.div``;
 const UploadPage = () => {
 	const [file, setFile] = useState(null);
 	const [fileData, setFileData] = useState([]);
+	const {plainFiles, openFilePicker} = useFilePicker({
+		multiple: false,
+		accept: '.csv'
+	});
 
 	const onDrop = useCallback((acceptedFiles) => {
 		const uploadFile = acceptedFiles[0];
 		if (uploadFile.name.endsWith('.csv')) {
 			setFile(uploadFile);
-			Papa.parse(uploadFile, {
-				header: true,
-				dynamicTyping: true,
-				skipEmptyLines: true,
-				complete: (result) => setFileData(result.data),
-			})
-
 		}
 	}, []);
 
@@ -41,7 +39,25 @@ const UploadPage = () => {
 		accept: '.csv',
 	})
 
+	useEffect(() => {
+		if (file) {
+			Papa.parse(file, {
+				header: true,
+				dynamicTyping: true,
+				skipEmptyLines: true,
+				complete: (result) => setFileData(result.data),
+			})
+		}
 
+	}, [file]);
+
+	useEffect(() => {
+		if (plainFiles.length) {
+			setFile(plainFiles[0])
+		}
+	}, [plainFiles])
+
+	useEffect(() => console.log(fileData), [fileData]);
 	return (
 		<PageWrapper>
 			<Dropzone {...getRootProps()}>
@@ -50,13 +66,14 @@ const UploadPage = () => {
 					file ? (
 						<p>{file.name}</p>
 					) : (
-						<p>Arraste e solte um arquivo csv para seleciona-lo.</p>
+						<>
+							<p>Arraste e solte um arquivo csv para seleciona-lo.</p>
+							<p>Ou se preferir </p>
+							<button onClick={openFilePicker}>Selecione o arquivo</button>
+						</>
 					)
 				}
 			</Dropzone>
-			{
-				fileData.length > 0 && ( <p>sucesso</p> )
-			}
 		</PageWrapper>
 	);
 }
