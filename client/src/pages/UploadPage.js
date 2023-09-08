b0a47035d569e88c21148960c9fb5932f5c446b0
Gustavo Felix <gfnascimento@inf.ufsm.br>
2023-09-08 18:23:40 -0300

    Implementando drop de arquivos

--- client/src/pages/UploadPage.js
+++ client/src/pages/UploadPage.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PageWrapper from '../components/PageWrapper';
+import { useDropzone } from 'react-dropzone';
+import styled from 'styled-components';
+
+const Dropzone = styled.div``;
+const DropInput = styled.div``;
+
 
 const UploadPage = () => {
+	const [file, setFile] = useState(null);
+
+	const onDrop = useCallback((acceptedFiles) => {
+		const uploadFile = acceptedFiles[0];
+		setFile(uploadFile);
+	}, []);
+
+	const { getRootProps, getInputProps } = useDropzone({
+		onDrop,
+		accept: 'csv',
+	})
+
 	return (
 		<PageWrapper>
 			<p>Pagina upload</p>
+			<Dropzone {...getRootProps()}>
+				<DropInput {...getInputProps}/>
+				<p>Arraste e solte um arquivo csv para seleciona-lo.</p>
+			</Dropzone>
+			{
+				file && (<p>Arquivo selecionado</p>)
+			}
 		</PageWrapper>
 	);
 }
