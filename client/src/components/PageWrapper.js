fae6528ae64c723e4e4b18782a5221ead68b164d
Gustavo Felix <gfnascimento@inf.ufsm.br>
2023-09-08 17:31:47 -0300

    Estilizando componentes do wrapper

--- client/src/components/PageWrapper.js
+++ client/src/components/PageWrapper.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const PageView = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: lightgray; 
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+const Title = styled.h1`
+	padding: 40px;
+`;
+
+const ContentView = styled.div`
+	border-radius: 20px;
+	margin: 40px;
+	background-color: white;
+	display: flex;
+	flex-direction: column;
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+`;
 
 const PageWrapper = ({children}) => {
 
 	return (
-		<div>
-			<h1>Titulo da pagina</h1>
-			<div>
+		<PageView>
+			<Title>Titulo da pagina</Title>
+			<ContentView>
 				{children}
-			</div>
-		</div>
+			</ContentView>
+		</PageView>
 	);
 };
 
