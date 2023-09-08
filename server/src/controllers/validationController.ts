76d23e02acd9720194b428c4f416fc3146cb7a5b
Gustavo Felix <gfnascimento@inf.ufsm.br>
2023-09-07 22:24:30 -0300

    Implementando rota de validação dos códigos

--- server/src/controllers/validationController.ts
+++ server/src/controllers/validationController.ts
@@ -1,17 +1,33 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
+const mountCodesAndValidation = (results: Array<any>, codes: Array<number>) => {
+	const validation = codes.map(code => {
+		const dbItem = results.find(item => item.code == code);
+		if ( dbItem ) {
+			delete dbItem.code
+			return { [code]: { valid: true, item: dbItem } };
+		}
+		return { [code]: { valid: false } };
+	});
+	return validation;
+};
+
 export const validateCodes = async (req: Request, res: Response)  => {
 	try {
 		const prisma = new PrismaClient();
+		const { codes } =  req.body;
 		const results = await prisma.products.findMany(
 			{
-				select: {
-					name: true,
+				where: {
+					code: {
+						in: codes,
+					}
 				}
 			}
 		);
-		res.json(results);
+		const codesAndValidation = mountCodesAndValidation(results, codes);
+		res.json(codesAndValidation);
 	} catch(e) {
 		console.error(e);
 		res.status(500).json({ error: 'Erro interno do servidor.' })
