306d1f74501da65186cd0acf3fa88acffe98f394
Gustavo Felix <gfnascimento@inf.ufsm.br>
2023-09-08 21:27:05 -0300

    Recebendo dados da pagina anterior

--- client/src/pages/ResultPage.js
+++ client/src/pages/ResultPage.js
@@ -1,6 +1,28 @@
+import React, { useEffect, useState } from 'react';
 import PageWrapper from '../components/PageWrapper';
+import { useLocation } from 'react-router-dom';
 
 const ResultPage = () => {
+	const [data, setData] = useState([]);
+	const location = useLocation();
+
+	const validateData = (unverifiedData) => {
+		console.log('Validando...', unverifiedData);
+	};
+
+	useEffect(() => {
+		const recivedData = location.state.fileData;
+		if (recivedData && recivedData.length) {
+			setData(recivedData);
+		}
+	}, [location]);
+
+	useEffect(() => {
+		if (data.length) {
+			validateData(data);
+		}
+	}, [data]);
+
 	return (
 		<PageWrapper>
 			<p>Pagina resultado</p>
