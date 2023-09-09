6bc3d2ffe4c0c2a552324b8de6be7fe04b97beae
Gustavo Felix <gfnascimento@inf.ufsm.br>
2023-09-09 00:43:01 -0300

    Integrando com api de validação

--- client/src/pages/ResultPage.js
+++ client/src/pages/ResultPage.js
@@ -1,14 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import PageWrapper from '../components/PageWrapper';
 import { useLocation } from 'react-router-dom';
 
+
 const ResultPage = () => {
 	const [data, setData] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 	const location = useLocation();
 
-	const validateData = (unverifiedData) => {
+	//	const mountRequestValidationBody = (codes) => ({codes: codes})
+	const mountRequestValidationOptions = useCallback((codes) => ({
+		method: 'GET', 
+		headers: {
+			'Content-Type': 'application/json', 
+
+		},
+	}), []);
+
+	const validateData = useCallback(async (unverifiedData) => {
 		console.log('Validando...', unverifiedData);
-	};
+		try {
+			const codes = unverifiedData.map( item => item.product_code );
+			const validationRequest = mountRequestValidationOptions(codes);
+			let apiUrl = 'http://127.0.0.1:3333/validate';
+			for(let i = 0; i < codes.length; i++) {
+				if (apiUrl.indexOf('?') === -1) {
+
+					apiUrl = `${apiUrl}?codes[]=${codes[i]}`
+				} else {
+
+					apiUrl = `${apiUrl}&codes[]=${codes[i]}`
+				}
+			}
+			const result = await fetch(`${apiUrl}`, validationRequest);
+			const data = await result.json();
+			console.log(data);
+		} catch(e) {
+			console.error('Erro ao validar dados: ', e);
+		} finally {
+			setIsLoading(false);
+		}
+	}, [mountRequestValidationOptions]);
 
 	useEffect(() => {
 		const recivedData = location.state.fileData;
@@ -21,11 +53,17 @@ const ResultPage = () => {
 		if (data.length) {
 			validateData(data);
 		}
-	}, [data]);
+	}, [data, validateData]);
 
 	return (
 		<PageWrapper>
-			<p>Pagina resultado</p>
+			{
+				isLoading ? (
+					<p>Validando dados...</p>
+				) : (
+					<p>Pagina resultado</p>
+				)
+			}
 		</PageWrapper>
 	);
 }
